b08fcc0b2288b401713d5e50c58ce36b37aa8a0b
Trojanf29 <93851043+Trojanf29@users.noreply.github.com>
2022-03-19 22:17:33 +0700

    Add files via upload
    1.0

---
+++ Bottom 3.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int n, A[100], i;
+int c;
+
+int main() {
+	scanf("%d", &n);
+	for (i = 0; i < n; i++)
+		scanf("%d", &A[i]);
+	c = 1;
+	for (i = 0; i < n / 2; i++)
+		if (A[i] != A[n - 1 - i])
+			c = 0;
+	printf("\nOUTPUT:\n");
+	printf("%d", c);
+	getch();
+	return 0;
+}
